27f4877702b62efcf575e17c76da299d56770373
mk87577 <mk87577@stud.uws.edu.pl>
2025-05-06 06:15:57 +0200

    Add files via upload

---
+++ IRundaQuizu.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLibrary1
+{
+    public interface IRundaQuizu
+    {
+        IKraj Kraj { get; }
+        string AktualnaPodpowiedź { get; }
+        int AktualnePunkty { get; }
+        bool CzyMoznaPokazacWiecejPodpowiedzi { get; }
+
+        void PokazNastepnaPodpowiedz();
+        bool SprawdzOdpowiedz(string odpowiedz);
+        bool CzyKoniecPodpowiedzi { get; }
+        void PrzejdzDoLatwiejszegoEtapu();
+    }
+}
